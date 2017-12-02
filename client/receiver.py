57aae75e8993a35ee8af99efe8424bd8d97c8f81
Alejandro Agustin B <alejandro.a.es@ieee.org>
2017-12-02 18:44:36 +0100

    no idea

--- client/receiver.py
+++ client/receiver.py
@@ -25,6 +25,7 @@ def receive_xbee_pin():
         frame.
         """
         print(data)
+        ser.write(b'hello')
 
     xbee = XBee(serial_port, callback=print_data)
 
@@ -39,20 +40,56 @@ def receive_xbee_pin():
 
 
 def validate_pin(pinet):
-    # TODO
     global base_url
     r = requests.post(base_url + "/pin/", json={"pin": pinet})
-    if r.status_code == 403:
-        print("Unauthorized")
-    elif r.status_code == 200:
-        print("Okok")
+    return r.status_code
+
+
+def processDoor(open):
+    global base_url
+    r = requests.post(base_url + "/pin/", json={"open": open})
+
+
+def evaluatePin(pinR):
+    status = validate_pin(pinR)
+    if status == 403:
+        return b'{"4":"KO"}'
+    elif status == 200:
+        return b'{"4":"OK"}'
     else:
-        print("Unhandled error")
+        return None
+
+
+
+
+def processData(line):
+    out = None
+    try:
+        jsonD = json.loads(line)
+        op = int(list(jsonD.keys())[0])
+        if op == 1:
+            processDoor(True)
+        elif op == 2:
+            processDoor(False)
+        elif op == 3:
+            out = evaluatePin(jsonD["3"])
+    except:
+        pass
+    return out
+
 
 def serialF():
     with serial.Serial('/dev/ttyUSB4', 9600) as ser:
         while True:
             line = ser.readline()
             print(line)
+            out = None
+            try:
+                out = processData(line[:-2].decode("utf-8") )
+            except:
+                pass
+            if out is not None:
+                print(out)
+                ser.write(out)
 
 serialF()
